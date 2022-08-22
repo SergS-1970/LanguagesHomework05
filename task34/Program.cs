7d6ba2865035aa4a2bcf0126b4a8bdb06af30458
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-23 02:38:21 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+int numberEven = 0;
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i] % 2 == 0) numberEven ++;
+}
+
+Console.WriteLine($"[{String.Join("; ", array)}]");
+
+Console.WriteLine($" Количество чётных элементов массива : {numberEven}");
